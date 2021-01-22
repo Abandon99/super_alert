09cd2a807cd843fb199c8e217759e69ab12e53f8
hanjunyi <han81594@163.com>
2021-01-23 06:07:26 +0800

    a easy start

---
+++ http.js
@@ -0,0 +1,39 @@
+const axios = require('axios')
+
+const ALERT_TIME = 10
+let todayHasCalled = false
+let shadowHous = (new Date()).getHours()
+
+const sendMessage = (msg) => {
+    return axios
+        .get('https://sc.ftqq.com/SCU153669T5eee827e527d52b70ed8d2c56c97a446600b1789ecae4.send?text=' + encodeURI(msg))
+}
+
+const callMe = () => {
+    const nowHour = (new Date()).getHours()
+    const gap = nowHour - shadowHous
+    if(gap < 0) todayHasCalled = false
+    if(gap > 1) shadowHous = nowHour - 1
+    if(todayHasCalled) {
+        console.log('发送过了')
+        return
+    } else {
+        if(nowHour >= ALERT_TIME) {
+            console.log('send message！！！！！！！========> 只有一次哦 很重要')
+            sendMessage('这是宇宙在喊你起床！！！赶紧的！！！')
+                .then(res => {
+                console.log('res => ', res)
+                })
+                .catch(err => {
+                    console.log('err ====>', err)
+                    clearInterval(sendMessageTimer)
+                })
+            todayHasCalled = true
+        } else {
+            console.log('还没到时间', (new Date()).getSeconds())
+        }
+    }
+    
+}
+
+const sendMessageTimer = setInterval(callMe, 1000 * 10)
